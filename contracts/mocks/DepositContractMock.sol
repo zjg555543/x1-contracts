7137e090aa73ced65cb85c3d68e45906b4a55194
jianguo <zjg555543@163.com>
2023-09-06 18:41:38 +0800

    update

--- contracts/mocks/DepositContractMock.sol
+++ contracts/mocks/DepositContractMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.20;
+pragma solidity 0.8.17;
 import "../lib/DepositContract.sol";
 import "hardhat/console.sol";
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -28,9 +28,8 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
+        version: "0.8.17",
         settings: {
-          evmVersion: 'paris',
           optimizer: {
             enabled: true,
             runs: 999999
@@ -87,15 +86,6 @@ module.exports = {
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
         path: "m/44'/60'/0'/0",
