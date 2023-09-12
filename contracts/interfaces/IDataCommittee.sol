342d412d2a3bbc51f9333c2684fa6677ca483ccc
jianguo <zjg555543@163.com>
2023-09-12 18:03:12 +0800

    update

---
+++ contracts/interfaces/IDataCommittee.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity ^0.8.20;
+
+interface IDataCommittee {
+    function verifySignatures(bytes32 hash, bytes memory signaturesAndAddrs) external view;
+}
